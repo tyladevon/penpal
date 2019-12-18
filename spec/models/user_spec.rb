636fc6c90771b85d54424491a356b01bbee4aadd
Mary Lang <mcl86@cornell.edu>
2019-12-18 15:22:25 -0700

    Setup
    Co-Authored-By: Scott Regenthal <freeheeling@users.noreply.github.com>
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,7 @@ describe User do
     it { should validate_presence_of :last_name }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :google_token }
   end
 
   describe 'relationships' do
