21afaf9ecf323e8474a5e545a656dc89145f998d
Mary Lang <mcl86@cornell.edu>
2019-12-18 15:16:54 -0700

    Setup
    Co-Authored-By: Scott Regenthal <freeheeling@users.noreply.github.com>
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+
+describe User do
+  describe 'attributes' do
+    it 'has attributes' do
+      user = User.create(first_name: 'Sarah', last_name: 'Lopp', email: 'slopp@gmail.com')
+
+      expect(user.first_name).to eq('Sarah')
+      expect(user.last_name).to eq('Lopp')
+      expect(user.email).to eq('slopp@gmail.com')
+    end
+  end
+  describe 'validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+  end
+
+  describe 'relationships' do
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,24 @@
+require 'simplecov'
+SimpleCov.start
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../config/environment', __dir__)
+
+require 'rspec/rails'
+require 'vcr'
+require 'webmock/rspec'
+
+VCR.configure do |config|
+  config.ignore_localhost = true
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.filter_sensitive_data("<YOUTUBE_API_KEY>") { ENV['YOUTUBE_API_KEY'] }
+  config.filter_sensitive_data("<GITHUB_TEST_TOKEN>") { ENV['GITHUB_TEST_TOKEN'] }
+end
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -60,4 +76,15 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+def stub_omniauth
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.add_mock(:github, {:uid => '12345', :credentials => {token: ENV['GITHUB_TEST_TOKEN']}, :info => {nickname: 'github_name'}})
 end
