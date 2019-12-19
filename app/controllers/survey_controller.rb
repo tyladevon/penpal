729336f74eb9aa53b6465501753601a6249020fa
Tyla Devon <tyladevon@gmail.com>
2019-12-19 13:58:54 -0700

    begin survey build

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,5 +1,9 @@
 class SurveyController < ApplicationController
   def index
+    @survey = Survey.new
+  end
+
+  def create
 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/landing', to: 'landing#index'
   get '/survey', to: 'survey#index'
+  post '/survey', to: 'survey#create'
   get '/auth/google_oauth2', as: 'google_login'
   get '/auth/google_oauth2/callback', to: 'users#create'
 end
