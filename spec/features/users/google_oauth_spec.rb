eec977bc8294ee163e9b464741d40b649acd0fc1
David Lopez <ldavid.x.lopezl@gmail.com>
2019-12-19 11:34:47 -0700

    Update user signup spec
    Co-Authored-By: Scott Regenthal <freeheeling@users.noreply.github.com>
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- spec/features/users/google_oauth_spec.rb
+++ spec/features/users/google_oauth_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
-describe 'As a registered user' do
+describe 'As a newly registered user' do
   it 'I can login with Google OAuth from the login page' do
     stub_omniauth
     visit '/'
     expect(page).to have_link('Sign in with Google')
     click_link('Sign in with Google')
 
-    expect(page).to have_content("Welcome, Huck!")
-    expect(page).to have_content("How are you feeling?")
+    expect(current_path).to eq(survey_path)
+    expect(page).to have_content('Login Successful')
+    expect(page).to have_content('Penpal wants to get to know you a little more')
   end
 end
