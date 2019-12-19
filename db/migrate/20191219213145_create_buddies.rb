1f9a48750328dc09bcd350f765324444056d89f6
Scott Regenthal <scottregenthal@gmail.com>
2019-12-19 15:23:35 -0700

    Create and migrate buddy table
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>

---
+++ db/migrate/20191219213145_create_buddies.rb
@@ -0,0 +1,13 @@
+class CreateBuddies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :buddies do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone_number
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
