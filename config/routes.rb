2ff0a0522d215ca34616cad684dbba036b7c39b6
Scott Regenthal <scottregenthal@gmail.com>
2019-12-19 14:07:50 -0700

    Add user destroy and show views with buttons and controller actions
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   get '/landing', to: 'landing#index'
   get '/survey', to: 'survey#index'
 
-  delete '/logout', to: 'sessions#destroy'
+  get '/profile', to: 'users#show'
+  delete '/users', to: 'users#destroy'
 
+  delete '/logout', to: 'sessions#destroy'
   get '/auth/google_oauth2', as: 'google_login'
   get '/auth/google_oauth2/callback', to: 'users#create'
 end
