3b8c97847d87893c4805d61fc97d172b80067f9c
Mary Lang <mcl86@cornell.edu>
2019-12-29 11:54:56 -0700

    Add spotify oauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,6 @@ Rails.application.routes.draw do
   get '/auth/google_oauth2', as: 'google_login'
   get '/auth/google_oauth2/callback', to: 'users#create'
 
+  get '/auth/spotify', as: 'spotify'
+  get '/auth/spotify/callback', to: 'spotify#create'
 end
