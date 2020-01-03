8cb6411e13ea76ee322187c1ddd20784fccf5c37
Tyla Devon <tyladevon@gmail.com>
2020-01-03 14:01:10 -0700

    Add resource facade to application controller for access.
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>
    Co-Authored-By: Scott Regenthal <freeheeling@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :resource_facade
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def resource_facade
+    @resource_facade ||= ResourceFacade.new(current_user)
+  end
 end
