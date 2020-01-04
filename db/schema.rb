963f928451c96567132fb4d1134919413fb4ed2e
Mary Lang <mcl86@cornell.edu>
2020-01-04 10:16:55 -0700

    Fix merge conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_03_222617) do
+ActiveRecord::Schema.define(version: 2020_01_04_171618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
