67b4bb0599f81d041da92a524fa75b8dbb41eca4
Mary Lang <mcl86@cornell.edu>
2020-01-04 11:39:08 -0700

    Add service and empty spec

---
+++ app/services/penpal_service.rb
@@ -0,0 +1,20 @@
+class PenpalService
+
+  def get_suggestion
+    get_json
+  end
+
+  private
+
+
+  def conn
+    Faraday.new(ENV['PENPAL_URL']) do |f|
+      f.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_json
+    response = conn.get
+    JSON.parse(response.body)
+  end
+end
---
+++ spec/features/services/penpal_service_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+
+describe 'Penpal service' do
+  it 'gets a suggested resource from penpal service api' do
+    
+
+
+  end
+end
