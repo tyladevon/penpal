3d45add0eaac2eb661ddb0dce6069fc52fb4e4ff
David Lopez <ldavid.x.lopezl@gmail.com>
2020-01-04 16:56:05 -0700

    Add resource specs
    Co-Authored-By: Mary Lang <mcat56@users.noreply.github.com>
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>
    Co-Authored-By: Scott Regenthal <freeheeling@users.noreply.github.com>

---
+++ spec/features/resources/media_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "user can receive a media resource" do
+  it "shows image" do
+    user = create(:user)
+    stub_user(user)
+
+    allow_any_instance_of(ResourceFacade).to receive(:suggestion).and_return("media")
+    media = Media.new({url: 'http://bit.ly/2FmmOoI'}, user)
+    allow_any_instance_of(ResourceFacade).to receive(:resource).and_return(media)
+
+    visit '/boost'
+
+    expect(page).to have_content("Penpal selected the following image to lift your spirits:")
+    expect(page).to have_css("img[src*='http://bit.ly/2FmmOoI']")
+  end
+end
---
+++ spec/features/resources/music_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "user can receive a music resource" do
+  it "displays song url" do
+    user = create(:user)
+    stub_user(user)
+
+    allow_any_instance_of(ResourceFacade).to receive(:suggestion).and_return("music")
+    music = Music.new({url: 'www.woo.co', song_name: 'Woop'}, user)
+    allow_any_instance_of(ResourceFacade).to receive(:resource).and_return(music)
+
+    visit '/boost'
+
+    expect(page).to have_content("Penpal selected the following song to lift your spirits:")
+    expect(page).to have_link("Woop")
+  end
+end
