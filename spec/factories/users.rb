4820a7e9e64d15054feeba5b1a7302eef8d2fde5
Scott Regenthal <scottregenthal@gmail.com>
2020-01-05 12:55:07 -0700

    Update factory attributes

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    email  { Faker::Internet.email }
+    email { Faker::Internet.email }
     first_name { Faker::Creature::Dog.name }
     last_name { Faker::Artist.name }
     google_token { '123455' }
+    spotify_token { '5544321' }
   end
-
 end
