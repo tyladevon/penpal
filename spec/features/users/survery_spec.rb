426f91260f8d84c5c4358d4d3077a30229d495c1
Mary Lang <mcl86@cornell.edu>
2020-01-07 13:27:30 -0700

    Add testing for restricted resources
    Co-Authored-By: David Lopez <ldavid.x.lopezl@gmail.com>

--- spec/features/users/survery_spec.rb
+++ spec/features/users/survery_spec.rb
@@ -54,4 +54,48 @@ describe 'User can fill out the survey' do
     expect(user.resource_preference[:activity]).to eq(true)
 
   end
+  it 'fills out information and submits' do
+    user = create(:user)
+    stub_user(user)
+
+    visit '/survey'
+
+    check 'Sad'
+    check 'Down'
+    check 'Before bed'
+    check 'Perform an activity I enjoy'
+    check 'Folk/Indie'
+    check 'Dogs'
+    check 'Celestial'
+    fill_in 'activity[activity_1]', with: 'Swimming'
+    fill_in 'activity[activity_2]', with: 'Coloring'
+
+    click_on 'Submit'
+
+    expect(current_path).to eq('/landing')
+    expect(page).to have_content('Preferences saved')
+
+    feelings = user.feeling_preferences.map do |pref|
+      pref.feeling
+    end
+
+    expect(feelings).to include('sad')
+    expect(feelings).to include('down')
+
+    expect(user.media_preference).to eq(nil)
+
+    expect(user.time_preference[:before_bed]).to eq(true)
+
+    expect(user.music_preferences).to eq([])
+
+    activities = user.activity_preferences.map do |pref|
+      pref.description
+    end
+    expect(activities).to include('Swimming')
+    expect(activities).to include('Coloring')
+    expect(user.resource_preference[:music]).to eq(false)
+    expect(user.resource_preference[:media]).to eq(false)
+    expect(user.resource_preference[:activity]).to eq(true)
+
+  end
 end
