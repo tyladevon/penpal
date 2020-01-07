321b14664c15d1b6393075ba019cf1a9d11cd715
Mary Lang <mcl86@cornell.edu>
2020-01-06 20:31:08 -0700

    Add email config in dev and prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,20 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.delivery_method = :smtp
+
+   config.action_mailer.smtp_settings = {
+     address:              'smtp.sendgrid.net',
+     port:                 '587',
+     domain:               'https://penpal-boost.herokuapp.com/',
+     user_name:            ENV["SENDGRID_USERNAME"],
+     password:             ENV["SENDGRID_PASSWORD"],
+     authentication:       'plain',
+     enable_starttls_auto: true
+   }
+   # Do not swallow errors in after_commit/after_rollback callbacks.
+   config.active_record.raise_in_transactional_callbacks = true
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
