282236c29c97170bfeba9cd36e9ba406ab319307
David Lopez <ldavid.x.lopezl@gmail.com>
2020-01-08 11:24:20 -0700

    Configure sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,18 +13,6 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
-  config.action_mailer.delivery_method = :smtp
-
-   config.action_mailer.smtp_settings = {
-     address:              'smtp.sendgrid.net',
-     port:                 '587',
-     domain:               'https://penpal-boost.herokuapp.com/',
-     user_name:            ENV["SENDGRID_USERNAME"],
-     password:             ENV["SENDGRID_PASSWORD"],
-     authentication:       'plain',
-     enable_starttls_auto: true
-   }
    # Do not swallow errors in after_commit/after_rollback callbacks.
    # config.active_record.raise_in_transactional_callbacks = true
 
@@ -105,4 +93,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "penpal-boost.herokuapp.com" }
+
+  config.action_mailer.smtp_settings = {
+     address:              'smtp.sendgrid.net',
+     port:                 '587',
+     domain:               'penpal-boost.herokuapp.com',
+     user_name:            ENV["SENDGRID_USERNAME"],
+     password:             ENV["SENDGRID_PASSWORD"],
+     authentication:       'plain',
+     enable_starttls_auto: true
+  }
 end
