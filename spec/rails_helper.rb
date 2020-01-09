0c8ece930ade35851fea5762632f515fc3947c0b
Mary Lang <mcl86@cornell.edu>
2020-01-09 13:55:55 -0700

    Change vcr config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,6 +16,7 @@ Sidekiq::Testing.fake!
 
 VCR.configure do |config|
   config.ignore_localhost = true
+  config.default_cassette_options = { :record => :none }
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
