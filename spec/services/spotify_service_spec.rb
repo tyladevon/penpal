fc2abcbd4d0e47551729f467a538eecdd79d3879
Mary Lang <mcl86@cornell.edu>
2020-01-09 11:52:38 -0700

    Get cassette

--- spec/services/spotify_service_spec.rb
+++ spec/services/spotify_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'Spotify Service can make api call' do
-  it 'can get a user token and refresh token', :vcr do
+  it 'can get a user token and refresh token' do
     user = create(:user, survey?: true, spotify_token: ENV['SPOTIFY_TEST_TOKEN'])
     stub_spotify_omniauth
 
